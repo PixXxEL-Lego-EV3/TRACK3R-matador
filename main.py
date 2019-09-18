f231b259ac126e8cbe1e27fd66ab19dadfad1843
roginaldosemog <roginaldosemog@gmail.com>
2019-09-18 14:14:35 -0300

    Teste nova aula

--- main.py
+++ main.py
@@ -9,44 +9,76 @@ from ev3dev2.sensor.lego import InfraredSensor
 from ev3dev2.led import Leds
 from ev3dev2.sound import Sound
 
-channel = 1
-
-tank_drive = MoveSteering(OUTPUT_C, OUTPUT_B)
+CHANNEL_OPPONENT = 1
 
 shooter = MediumMotor(OUTPUT_A)
 sensor = InfraredSensor(INPUT_4)
+tank_drive = MoveSteering(OUTPUT_C, OUTPUT_B)
 
 # Add logging
 logging.basicConfig(level=logging.DEBUG, format="%(lineno)s: %(message)s")
 log = logging.getLogger(__name__)
 
 # Functions
-def corno_ahead():
-    head = sensor.heading(channel)
-    dist = sensor.distance(channel)
+def shoot_ball():
+    shooter.on_for_rotations(SpeedPercent(100), 3)
 
-    log.info("heading: ")
-    log.info(head)
-    log.info("distance: ")
-    log.info(dist)
+# testar
+def shoot_ball_forever():
+    shooter.on(SpeedPercent(100))
+    # ou
+    # shooter.run_forever()
+
+# testar
+def stop_shooting():
+    shooter.stop()
+
+def spin_matador():
+    tank_drive.on_for_rotations(-100, SpeedPercent(80), 1)
+
+# testar
+def spin_matador_forever():
+    tank_drive.on(-100, SpeedPercent(80))
+
+# testar
+def stop_spinning():
+    tank_drive.off()
+    # ou
+    # tank = MotorSet(?)
+    # tank.off()
+
+def corno_ahead():
+    head = sensor.heading(CHANNEL_OPPONENT)
+    dist = sensor.distance(CHANNEL_OPPONENT)
 
     if (dist is not None and dist < 50):
         if (head is not None and head > -5 and head < 5):
-            shooter.on_for_rotations(SpeedPercent(100), 3)
-
-def roda_roda_jequiti():
-    tank_drive.on_for_rotations(-100, SpeedPercent(80), 1)
+            return True
+    
+    return False
 
 # Code
 def main():
     log.info("Starting MATADOR")
-    # shooter.reset()
 
+    # while True:
+    #     spin_matador()
+    #     corno_ahead()
+
+    # testar
     while True:
-        roda_roda_jequiti()
-        corno_ahead()
+        if corno_ahead():
+            stop_spinning():
+            shoot_ball_forever():
+        else
+            stop_shooting():
+            spin_matador_forever():
     
     log.info("Finishing MATADOR")
 
 if __name__ == '__main__':
     main()
+
+# Utilidades
+# shooter.reset()
+# log.info("Heading: " + head) # testar
