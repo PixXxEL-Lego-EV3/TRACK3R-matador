01b7d5ef6e92b06b514080fee275a32c86846533
Pedro Ivo Andrade <andradepedroivo@gmail.com>
2019-10-23 15:29:52 -0300

    UUUhhh

--- main.py
+++ main.py
@@ -25,18 +25,22 @@ btn = Button()
 logging.basicConfig(level=logging.DEBUG, format="%(lineno)s: %(message)s")
 log = logging.getLogger(__name__)
 
+
 # Functions
 def shoot_ball():
     shooter.on_for_rotations(SpeedPercent(100), 3)
+
 
 # testar
 def shoot_ball_forever():
     shooter.on(SpeedPercent(100))
 
+
 # Para de atirar
 def stop_shooting():
     log.info("Stop shooting")
     shooter.stop()
+
 
 # Roda n vezes
 def spin_matador(turns):
@@ -48,15 +52,22 @@ def run_matador_seconds(seconds):
     tank_drive.on_for_seconds(0, SpeedPercent(50), seconds)
 
 
+def engata_re(seconds):
+    tank_drive.on_for_seconds(0, SpeedPercent(-70), seconds)
+
+
 def spin_matador_degrees_right(degrees):
     tank_drive.on_for_degrees(-100, SpeedPercent(60), degrees)
+
 
 def spin_matador_degrees_left(degrees):
     tank_drive.on_for_degrees(100, SpeedPercent(60), degrees)
 
+
 def spin_matador_forever():
     log.info("Spinning robot forever")
     tank_drive.on(100, SpeedPercent(50))
+
 
 # testar
 
@@ -76,14 +87,18 @@ def corno_ahead():
 
     return False
 
+
 # Code
 print("Starting MATADOR")
 
 while not btn.any():
     sleep(0.01)
 
+engata_re(11)
+spin_matador_degrees_left(650)
+
 cont = 0
-while(cont < 12):
+while (cont < 6):
     if not corno_ahead():
         run_matador_seconds(1)
         if corno_ahead():
@@ -91,21 +106,6 @@ while(cont < 12):
     else:
         shoot_ball()
     cont += cont + 1
-    
-# run_matador_seconds(7*2)
-spin_matador_degrees_left(300)
-
-cont = 0
-while(cont < 9):
-    if not corno_ahead():
-        run_matador_seconds(1)
-        if corno_ahead():
-            shoot_ball()
-    else:
-        shoot_ball()
-    cont += cont + 1
-
-# run_matador_seconds(5)
 
 while True:
     log.info("Distancia: " + str(sensor.value(CHANNEL_OPPONENT)))
@@ -117,7 +117,44 @@ while True:
         stop_shooting()
         spin_matador_forever()
 
-log.info("Finishing MATADOR")
+########################################################
+
+# cont = 0
+# while(cont < 12):
+#     if not corno_ahead():
+#         run_matador_seconds(1)
+#         if corno_ahead():
+#             shoot_ball()
+#     else:
+#         shoot_ball()
+#     cont += cont + 1
+
+# # run_matador_seconds(7*2)
+# spin_matador_degrees_left(300)
+
+# cont = 0
+# while(cont < 9):
+#     if not corno_ahead():
+#         run_matador_seconds(1)
+#         if corno_ahead():
+#             shoot_ball()
+#     else:
+#         shoot_ball()
+#     cont += cont + 1
+
+# # run_matador_seconds(5)
+
+# while True:
+#     log.info("Distancia: " + str(sensor.value(CHANNEL_OPPONENT)))
+
+#     if corno_ahead():
+#         stop_spinning()
+#         shoot_ball()
+#     else:
+#         stop_shooting()
+#         spin_matador_forever()
+
+# log.info("Finishing MATADOR")
 
 # Utilidades
 # shooter.reset()
